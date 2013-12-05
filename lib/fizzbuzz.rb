65de568ff31ddc60631f57a5b10a0c6397d3bb01
alonzae <apruitt@chicagotechacademy.org>
2013-12-05 17:34:59 -0600

    homework w/Mr.Rush

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,13 @@
+class Calculator 
+	def calculate(num)
+		if num == 1
+			return 1
+		elsif num == 2
+			return 2
+		elsif num ==3
+			return "fizz"
+		elsif num == 4
+			return 4
+		end
+	end
+end
